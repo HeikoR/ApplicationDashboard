e346d8bc6429af6c4e7ea3252d9751f793bff930
Heiko Risser <h.risser@gmail.com>
2013-02-18 15:07:27 +0200

    fix examples - broke with latest webserver changes. Counter functions can now be chained.

--- examples/create-simple-webserver.js
+++ examples/create-simple-webserver.js
@@ -2,7 +2,9 @@
 // This creates a stats server 
 require('./create-simple-stats-server');
 
-var Webserver = require('../').Webserver;
+var Webserver = require('../')
+	.WebServer()
+	.startCollector();
 
 
 
